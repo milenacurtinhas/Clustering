a1a0f34de5dbf64395546d1e1318ee0468758d2e
Luiz Machado <luizfelmach@gmail.com>
2024-04-12 14:22:37 -0300

    refatorando

--- src/reading.c
+++ src/reading.c
@@ -4,7 +4,7 @@
 
 #include "vertex.h"
 
-int line_dimension(char *line) {
+int vertex_dimension(char *line) {
     int result = 0;
     while (*line) {
         if (*line == ',') result++;
@@ -14,44 +14,30 @@ int line_dimension(char *line) {
 }
 
 int read_file_of_vertexes(FILE *file, Vector vertexes) {
-    char *line_buf = NULL;
-    size_t line_buf_size = 0;
-    ssize_t line_size = 0;
+    char *line;
+    size_t size = 0;
+    ssize_t read = getline(&line, &size, file);
+    int dimension = vertex_dimension(line);
 
-    if (!file) {
-        printf("ERROR: couldn't find the file given!\n");
-    }
-
-    /* Loop through until we are done with the file. */
-    line_size = getline(&line_buf, &line_buf_size, file);
-    int dimension = line_dimension(line_buf);
-
-    while (line_size >= 0) {
-        double *coordinates = calloc(dimension, sizeof(double));
-        char *name = strdup(strtok(line_buf, ",\n"));
-
+    while (read != -1) {
+        double *coordinates = malloc(dimension * sizeof(double));
+        char *name = strdup(strtok(line, ",\n"));
         char *ptr, *number_char = strtok(NULL, ",\n");
+
         int i = 0;
         while (number_char != NULL) {
             double number = strtod(number_char, &ptr);
             coordinates[i] = number;
-
             i++;
             number_char = strtok(NULL, ",\n");
         }
 
         Vertex v = vertex_init(name, coordinates);
-        // double *coord = vertex_get_coordinates(v);
-
-        // printf("%s: %lf, %lf\n", vertex_get_name(v), coord[0], coord[1]);
-
         vector_push(vertexes, &v);
 
-        line_size = getline(&line_buf, &line_buf_size, file);
+        read = getline(&line, &size, file);
     }
 
-    /* Free the allocated line buffer */
-    free(line_buf);
-
+    free(line);
     return dimension;
 }
